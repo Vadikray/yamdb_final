60e17d32a3cf7bfa912c8105c113fcb52753e110
Вадим Конюшков <konuskov92@gmail.com>
2023-02-15 01:47:40 +0500

    CI v1233

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = get_random_secret_key()
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['84.252.140.229', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['51.250.102.201', '127.0.0.1', 'localhost', '*', ]
 # ALLOWED_HOSTS = []
 
 
@@ -72,12 +72,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': os.getenv('DB_PORT', '5432')
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
