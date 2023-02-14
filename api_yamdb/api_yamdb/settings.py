550fce200df10a1da2e604dd30c917c1b9172e98
Вадим Конюшков <konuskov92@gmail.com>
2023-02-15 01:51:19 +0500

    CI start

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,7 +17,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['51.250.102.201', '127.0.0.1', 'localhost', '*', ]
 # ALLOWED_HOSTS = []
 
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
